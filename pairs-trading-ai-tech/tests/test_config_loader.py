2b787a3d131771f56ad5ee5a0299c1ac8a9b4c32
Aditi Seshadri <aditi.seshadri@gmail.com>
2026-06-19 11:05:41 +0530

    Run config test suite part 1.

--- pairs-trading-ai-tech/tests/test_config_loader.py
+++ pairs-trading-ai-tech/tests/test_config_loader.py
@@ -1,7 +1,13 @@
 import json
 import pytest
-from src.config_loader import load_universe_config
+from src.config_loader import (
+    load_universe_config,
+    load_run_config
+)
 
+#Universe ticker checks
+
+#Valid case
 def test_load_universe_config_valid(
     tmp_path
 ):
@@ -15,7 +21,8 @@ def test_load_universe_config_valid(
     result = load_universe_config(path)
     
     assert result["tickers"] == ["AAPL","MSFT","NVDA"]
-    
+
+#Ticker key missing
 def test_load_universe_config_missing_key(
     tmp_path
 ):
@@ -29,6 +36,7 @@ def test_load_universe_config_missing_key(
     with pytest.raises(ValueError):
         load_universe_config(path)
     
+#Tickers was not a list
 def test_load_universe_config_tickers_bad_type(
     tmp_path
 ):
@@ -42,6 +50,7 @@ def test_load_universe_config_tickers_bad_type(
     with pytest.raises(TypeError):
         load_universe_config(path)
         
+#Tickers had duplicate values
 def test_load_universe_config_duplicate_tickers(
     tmp_path
 ):
@@ -55,6 +64,7 @@ def test_load_universe_config_duplicate_tickers(
     with pytest.raises(ValueError):
         load_universe_config(path)
 
+#tickers list was empty
 def test_load_universe_config_empty_tickers(
     tmp_path
 ):
@@ -68,6 +78,7 @@ def test_load_universe_config_empty_tickers(
     with pytest.raises(ValueError):
         load_universe_config(path)
 
+#A ticker was an empty string
 def test_load_universe_config_empty_str_tickers(
     tmp_path
 ):
@@ -81,6 +92,7 @@ def test_load_universe_config_empty_str_tickers(
     with pytest.raises(ValueError):
         load_universe_config(path)
 
+#a ticker given was not a string
 def test_load_universe_config_non_str_tickers(
     tmp_path
 ):
@@ -93,3 +105,263 @@ def test_load_universe_config_non_str_tickers(
     
     with pytest.raises(TypeError):
         load_universe_config(path)
+
+#Run config date check
+#Valid case for start_date, end_date and pvalue_threshold
+def test_load_run_config_valid_date_pvalue(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end_date":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":0.05,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+        
+    results = load_run_config(path)
+    
+    assert results["date_range"]["start_date"] == "2020-01-01"
+    assert results["date_range"]["end_date"] == "2024-12-31"
+    assert results["pairs_selection"]["pvalue_threshold"] == 0.05
+    
+#Missing start_date
+def test_load_run_config_missing_start_date(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start":"2020-01-01",
+            "end_date":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":0.05,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+    
+    with pytest.raises(ValueError):
+        load_run_config(path)
+
+#Missing end_date
+def test_load_run_config_missing_end_date(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":0.05,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+    
+    with pytest.raises(ValueError):
+        load_run_config(path)
+
+#End_date less than or equal to start_date
+def test_load_run_config_wrong_end_date(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end_date":"2019-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":0.05,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+    
+    with pytest.raises(ValueError):
+        load_run_config(path)
+
+#Missing pvalue_threshold
+def test_load_run_config_missing_pvalue_threshold(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end_date":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue":0.05,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+        
+    with pytest.raises(ValueError):
+        load_run_config(path)
+        
+#Non-numeric pvalue_threshold
+def test_load_run_config_non_numeric_pvalue(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end_date":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":"0.05",
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+    
+    with pytest.raises(TypeError):
+        load_run_config(path)
+        
+#negative_pvalue
+def test_load_run_config_negative_pvalue(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end_date":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":-1.05,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+    
+    with pytest.raises(ValueError):
+        load_run_config(path)
+        
+#Large positive pvalue over 1
+def test_load_run_config_abnormal_pvalue(
+    tmp_path
+):
+    
+    data = {
+        "date_range":{
+            "start_date":"2020-01-01",
+            "end_date":"2024-12-31"
+            },
+        "pairs_selection":{
+            "top_n":5,
+            "min_observations":60,
+            "min_correlation":0.4,
+            "pvalue_threshold":1.5,
+            "min_avg_dollar_volume":10000000
+        },
+        "data_loader":{
+            "missing_threshold":0.05
+        },
+        "data_files":{},
+        "spread_model":{},
+        "backtest":{}
+    }
+    path = tmp_path/"run_config.json"
+    
+    with open(path, 'w') as file:
+        json.dump(data,file)
+    
+    with pytest.raises(ValueError):
+        load_run_config(path)
